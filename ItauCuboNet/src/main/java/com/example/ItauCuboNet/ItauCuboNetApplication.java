86c4d285ad39eab8afefff61b3ebb5b44970a562
lucas <lucasgit.breeding934@passinbox.com>
2024-06-10 10:58:20 -0300

    Spring-Boot project init

---
+++ ItauCuboNet/src/main/java/com/example/ItauCuboNet/ItauCuboNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.ItauCuboNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauCuboNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauCuboNetApplication.class, args);
+	}
+
+}
